81f6c74a1ffaff40fcc95c7fcbcf87b9ecc2605a
HyeonSeok <gustjr2332@naver.com>
2024-03-25 15:41:21 +0900

    inital setting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import GamePage from './pages/GamePage';
+
+function App() {
+  return (
+    <div className="App">
+      <GamePage />
+    </div>
+  );
+}
+
+export default App;
